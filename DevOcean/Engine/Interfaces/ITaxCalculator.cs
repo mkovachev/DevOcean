630cc08b411df009a41d4d4200c323602dc894ba
mkovachev <mkovachev6@gmail.com>
2021-07-25 23:29:27 +0300

    created

---
+++ DevOcean/Engine/Interfaces/ITaxCalculator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DevOcean.Engine.Interfaces
+{
+    public interface ITaxCalculator
+    {
+        public string CalculateTax(IList<string> taxData);
+    }
+}
---
+++ DevOcean/Engine/TaxCalculator.cs
@@ -0,0 +1,47 @@
+﻿using DevOcean.Data.Enums;
+using DevOcean.Engine.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace DevOcean.Engine
+{
+    public class TaxCalculator : ITaxCalculator
+    {
+        private const decimal InitialTaxCargo = 10000;
+        private const decimal TaxPer1000LightMilesCargo = 1000;
+        private const decimal TaxAmortizationPerYearCargo = 736;
+
+        private const decimal InitialTaxFamily = 5000;
+        private const decimal TaxPer1000LightMilesFamily = 100;
+        private const decimal TaxAmortizationPerYearFamily = 355;
+
+        private readonly IInputProcessorHelper inputProcessorHelper;
+
+        public TaxCalculator(IInputProcessorHelper inputProcessorHelper)
+        {
+            this.inputProcessorHelper = inputProcessorHelper;
+        }
+
+        public string CalculateTax(IList<string> taxData)
+        {
+            decimal result;
+
+            var spaceshipType = Enum.Parse(typeof(SpaceshipType), this.inputProcessorHelper.CapitalizeFirstletter(taxData[0]));
+            var yearOfPurchase = int.Parse(taxData[1]);
+            var yearForTaxCalculation = int.Parse(taxData[2]);
+            var milesTraveled = this.inputProcessorHelper.GetDigitsAfterThousandSeparator(taxData[3]);
+
+            switch (spaceshipType)
+            {
+                case SpaceshipType.Cargo:
+                    result = InitialTaxCargo + (int.Parse(milesTraveled) * TaxPer1000LightMilesCargo) - ((yearForTaxCalculation - yearOfPurchase) * TaxAmortizationPerYearCargo); break;
+                case SpaceshipType.Family:
+                    result = InitialTaxFamily + (int.Parse(milesTraveled) * TaxPer1000LightMilesFamily) - ((yearForTaxCalculation - yearOfPurchase) * TaxAmortizationPerYearFamily); break;
+                default: return null;
+            }
+
+            return result.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
