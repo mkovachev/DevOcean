a4707977ddaa9f9be57061cfcc03c5bbf4bd1d2b
mkovachev <mkovachev6@gmail.com>
2021-07-25 20:09:34 +0300

    created

---
+++ DevOcean/Data/Models/SpaceshipCargo.cs
@@ -0,0 +1,9 @@
+﻿using DevOcean.Data.Enums;
+
+namespace DevOcean.Data.Models
+{
+    public class SpaceshipCargo : Spaceship
+    {
+        public override SpaceshipType Type => SpaceshipType.Cargo;
+    }
+}
