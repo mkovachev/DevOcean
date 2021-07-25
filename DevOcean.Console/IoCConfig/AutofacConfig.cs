930787ad4aab203b6a8a2946a673895852251fe2
mkovachev <mkovachev6@gmail.com>
2021-07-25 21:54:42 +0300

    created

---
+++ DevOcean.Console/IoCConfig/AutofacConfig.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using DevOcean.Engine;
+using DevOcean.Engine.Interfaces;
+using System.Reflection;
+
+namespace DevOcean.Console.IoCConfig
+{
+    public sealed class AutofacConfig : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var currentAssembly = Assembly.Load("DevOcean");
+
+            builder.RegisterAssemblyTypes(currentAssembly)
+                   .AsImplementedInterfaces();
+
+            builder.RegisterType<InputProcessorHelper>().As<IInputProcessorHelper>().SingleInstance();
+            builder.RegisterType<InputProcessor>().As<IInputProcessor>().SingleInstance();
+
+            builder.RegisterType<SpaceEngine>().As<IEngine>().SingleInstance();
+
+        }
+    }
+}
---
+++ DevOcean.Console/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using DevOcean.Console.IoCConfig;
+using DevOcean.Engine.Interfaces;
+
+namespace DevOcean.Console
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new AutofacConfig());
+            var container = builder.Build();
+
+            var engine = container.Resolve<IEngine>();
+            engine.Start();
+        }
+    }
+}
