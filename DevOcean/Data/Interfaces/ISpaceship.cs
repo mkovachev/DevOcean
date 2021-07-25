33ab7287e3ef27010aabafc79261167c7203a764
mkovachev <mkovachev6@gmail.com>
2021-07-25 20:10:23 +0300

    created

---
+++ DevOcean/Data/Interfaces/ISpaceship.cs
@@ -0,0 +1,11 @@
+﻿using DevOcean.Data.Enums;
+
+namespace DevOcean.Data.Interfaces
+{
+    public interface ISpaceship
+    {
+        string PurchaseDate { get; }
+
+        SpaceshipType Type { get; }
+    }
+}
