ff190b20188e6c9d0e8d1c23b08730e622b0c71a
mkovachev <mkovachev6@gmail.com>
2021-07-25 20:36:29 +0300

    created

---
+++ DevOcean/Engine/InputProcessor.cs
@@ -0,0 +1,57 @@
+﻿using DevOcean.Engine.Interfaces;
+using DevOcean.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOcean.Engine
+{
+    public class InputProcessor : IInputProcessor
+    {
+        private const int InputParamsCount = 4;
+
+        private readonly IWriter writer;
+        private readonly IReader reader;
+
+        public InputProcessor(IReader reader, IWriter writer)
+        {
+            this.reader = reader;
+            this.writer = writer;
+        }
+
+        public List<string> ReadInput()
+        {
+            var taxData = new List<string>();
+            try
+            {
+                this.writer.Write("Please type in a spaceship type: ");
+                var spaceshipType = this.reader.ReadLine().ToLower().Trim();
+
+                this.writer.Write("Please type in a purchase date: ");
+                var purchaseDate = this.reader.ReadLine().ToLower().Trim();
+
+                this.writer.Write("Please type in an year of tax calculation: ");
+                var yearOfTaxCalculation = this.reader.ReadLine().ToLower().Trim();
+
+                this.writer.Write("Please type in a light miles travaled: ");
+                var lightMilesTravaled = this.reader.ReadLine().ToLower().Trim();
+
+                taxData.Add(spaceshipType);
+                taxData.Add(purchaseDate);
+                taxData.Add(yearOfTaxCalculation);
+                taxData.Add(lightMilesTravaled);
+            }
+            catch (Exception ex)
+            {
+                this.writer.WriteLine($"{ex.Message}");
+            }
+
+            return taxData;
+        }
+
+        public bool IsValidateInput(List<string> input) => input.Count == InputParamsCount
+                && !input.Any(i => string.IsNullOrWhiteSpace(i))
+                && !input.Any(i => string.IsNullOrEmpty(i))
+                && !input.All(i => i.Any(x => !char.IsDigit(x)));
+    }
+}
---
+++ DevOcean/Engine/Interfaces/IInputProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DevOcean.Engine.Interfaces
+{
+    public interface IInputProcessor
+    {
+        public List<string> ReadInput();
+
+        public bool IsValidateInput(List<string> input);
+    }
+}
