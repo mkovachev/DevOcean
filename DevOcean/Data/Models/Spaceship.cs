9657751c4cd13c64a744eb5f83605e6250c3ecf0
mkovachev <mkovachev6@gmail.com>
2021-07-25 20:09:02 +0300

    created

---
+++ DevOcean/Data/Models/Spaceship.cs
@@ -0,0 +1,14 @@
+﻿using DevOcean.Data.Enums;
+using DevOcean.Data.Interfaces;
+using System;
+
+namespace DevOcean.Data.Models
+{
+    public abstract class Spaceship : ISpaceship
+    {
+        public string PurchaseDate => DateTime.Now.ToString("MM/dd/yyyy");
+
+        public virtual SpaceshipType Type => SpaceshipType.Default;
+
+    }
+}
