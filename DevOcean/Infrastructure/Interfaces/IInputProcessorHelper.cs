1e1fed162b59644dc6c9218d653fa076f6fd6b7e
mkovachev <mkovachev6@gmail.com>
2021-07-25 20:40:30 +0300

    created

---
+++ DevOcean/Infrastructure/Interfaces/IInputProcessorHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace DevOcean.Infrastructure.Interfaces
+{
+    public interface IInputProcessorHelper
+    {
+        public string CapitalizeFirstletter(string input);
+
+        public string GetDigitsAfterThousandSeparator(string input);
+    }
+}
