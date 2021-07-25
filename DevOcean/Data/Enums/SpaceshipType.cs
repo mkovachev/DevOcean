3b51435af94649231fa49fc95d7a49a69ec1382b
mkovachev <mkovachev6@gmail.com>
2021-07-25 20:11:47 +0300

    created

---
+++ DevOcean/Data/Enums/SpaceshipType.cs
@@ -0,0 +1,9 @@
+﻿namespace DevOcean.Data.Enums
+{
+    public enum SpaceshipType
+    {
+        Default = 0,
+        Cargo = 1,
+        Family = 2
+    }
+}
---
+++ DevOcean/Data/Models/SpaceshipFamily.cs
@@ -0,0 +1,9 @@
+﻿using DevOcean.Data.Enums;
+
+namespace DevOcean.Data.Models
+{
+    public class SpaceshipFamily : Spaceship
+    {
+        public override SpaceshipType Type => SpaceshipType.Family;
+    }
+}
