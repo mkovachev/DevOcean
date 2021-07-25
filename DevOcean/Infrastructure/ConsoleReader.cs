2dc0a4d287fecefbe7be227492992f5d8db442b0
mkovachev <mkovachev6@gmail.com>
2021-07-25 20:22:55 +0300

    created

---
+++ DevOcean/Infrastructure/ConsoleReader.cs
@@ -0,0 +1,13 @@
+﻿using DevOcean.Infrastructure.Interfaces;
+using System;
+
+namespace DevOcean.Infrastructure
+{
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ DevOcean/Infrastructure/ConsoleWriter.cs
@@ -0,0 +1,18 @@
+﻿using DevOcean.Infrastructure.Interfaces;
+using System;
+
+namespace DevOcean.Infrastructure
+{
+    public class ConsoleWriter : IWriter
+    {
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Write(string message)
+        {
+            Console.Write(message);
+        }
+    }
+}
