f40772e8ff096389a9716f21461129f8c62695fa
mkovachev <mkovachev6@gmail.com>
2021-07-26 08:55:34 +0300

    removed unused tests

--- DevOcean.Tests/Engine/SpaceEngineTests.cs
+++ DevOcean.Tests/Engine/SpaceEngineTests.cs
@@ -18,64 +18,6 @@ namespace DevOcean.Tests.Engine
             this.sut = new SpaceEngine(writer, inputProcessor, taxCalculator);
         }
 
-        //[Theory]
-        //[InlineData("family", "2300", "2307", "2344", 2715)]
-        //[InlineData("cargo", "2332", "2369", "344789", 326768)]
-        //public void Start_Should_Returns_CalculatedTax_Correctly(
-        //   string spaceshipType,
-        //   string yearOfPurchase,
-        //   string yearForTaxCalculation,
-        //   string milesTravele,
-        //    int expected)
-        //{
-        //    // Arange
-        //    var taxData = new List<string>()
-        //    {
-        //        spaceshipType,
-        //        yearOfPurchase,
-        //        yearForTaxCalculation,
-        //        milesTravele
-        //    };
-
-        //    this.inputProcessor.ReadInput().Returns(taxData);
-        //    this.inputProcessor.IsValidateInput(taxData);
-        //    var result = this.taxCalculator.CalculateTax(taxData);
-
-        //    // Act
-        //    this.sut.Start();
-
-        //    // Assert
-        //    Assert.Equal(expected, int.Parse(result));
-        //}
-
-        //[Theory]
-        //[InlineData("family", "2300", "2307", "2344")]
-        //public void Start_Should_Returns_CalculatedTax_OfType_String(
-        //    string spaceshipType,
-        //    string yearOfPurchase,
-        //    string yearForTaxCalculation,
-        //    string milesTravele)
-        //{
-        //    // Arange
-        //    var taxData = new List<string>()
-        //    {
-        //        spaceshipType,
-        //        yearOfPurchase,
-        //        yearForTaxCalculation,
-        //        milesTravele
-        //    };
-
-        //    this.inputProcessor.ReadInput().Returns(taxData);
-        //    this.inputProcessor.IsValidateInput(taxData);
-        //    var result = this.taxCalculator.CalculateTax(taxData);
-
-        //    // Act
-        //    this.sut.Start();
-
-        //    // Assert
-        //    Assert.IsType<string>(result);
-        //}
-
         [Theory]
         [InlineData(null, "", " ", "2344")]
         public void Start_Should_Returns_IsValidInput_False_If_Input_Contains_Null_WhiteSpace_NonDigits_Parameters(
--- DevOcean.Tests/Engine/TaxCalculatorTests.cs
+++ DevOcean.Tests/Engine/TaxCalculatorTests.cs
@@ -17,65 +17,6 @@ namespace DevOcean.Tests.Engine
             this.sut = new TaxCalculator(inputProcessorHelper);
         }
 
-        //[Theory]
-        //[InlineData("family", null, "2307", "2344")]
-        //[InlineData("family", "2300", null, "2344")]
-        //public void CalculateTax_Should_Returns_Null_When_Input_Paramaters_Are_Invalid(
-        //   string spaceshipType,
-        //   string yearOfPurchase,
-        //   string yearForTaxCalculation,
-        //   string milesTraveled)
-        //{
-        //    //Arange
-        //    this.inputProcessorHelper.CapitalizeFirstletter(spaceshipType)
-        //        .Returns(char.ToUpper(spaceshipType[0]) + spaceshipType.Substring(1));
-        //    this.inputProcessorHelper.GetDigitsAfterThousandSeparator(milesTraveled)
-        //        .Returns(int.Parse(milesTraveled).ToString("n", CultureInfo.GetCultureInfo("en-US")).Split(",")[0]);
-
-        //    var taxData = new List<string>()
-        //    {
-        //        spaceshipType,
-        //        yearOfPurchase,
-        //        yearForTaxCalculation,
-        //        milesTraveled
-        //    };
-
-        //    //Act
-        //    var result = this.sut.CalculateTax(taxData);
-
-        //    //Assert
-        //    Assert.Null(result);
-        //}
-
-        //[Theory]
-        //[InlineData(null, "2300", "2307", "2344")]
-        //[InlineData("family", "2300", "2307", null)]
-        //public void CalculateTax_Returns_Null_When_TaxData_Has_Null_Parameters(
-        //   string spaceshipType,
-        //   string yearOfPurchase,
-        //   string yearForTaxCalculation,
-        //   string milesTraveled)
-        //{
-        //    //Arange
-        //    this.inputProcessorHelper.CapitalizeFirstletter(spaceshipType).Returns(x => null);
-
-        //    this.inputProcessorHelper.GetDigitsAfterThousandSeparator(milesTraveled).Returns(x => null);
-
-        //    var taxData = new List<string>()
-        //    {
-        //        spaceshipType,
-        //        yearOfPurchase,
-        //        yearForTaxCalculation,
-        //        milesTraveled
-        //    };
-
-        //    //Act
-        //    var result = this.sut.CalculateTax(taxData);
-
-        //    //Assert
-        //    Assert.Null(result);
-        //}
-
         [Theory]
         [InlineData("family", "2300", "2307", "2344", 2715)]
         [InlineData("cargo", "2332", "2369", "344789", 326768)]
