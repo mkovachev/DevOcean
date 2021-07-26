65d16673bb41905af0c9ce673f33c18e184a6c1c
mkovachev <mkovachev6@gmail.com>
2021-07-26 12:15:31 +0300

    moved

--- DevOcean/Engine/Interfaces/IInputProcessorHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevOcean.Engine.Interfaces
-{
-    public interface IInputProcessorHelper
-    {
-        public string CapitalizeFirstLetter(string input);
-
-        public string GetDigitsAfterThousandSeparator(string input);
-    }
-}
