db6c201409d1ee2ae664c2076c80a2b0a82b079c
mkovachev <mkovachev6@gmail.com>
2021-07-26 12:33:57 +0300

    fix digits separator method

--- DevOcean/Infrastructure/InputHelper.cs
+++ DevOcean/Infrastructure/InputHelper.cs
@@ -8,6 +8,6 @@ namespace DevOcean.Engine
         public string CapitalizeFirstLetter(string input) => char.ToUpper(input[0]) + input.Substring(1);
 
         public string GetDigitsAfterThousandSeparator(string input)
-            => int.Parse(input).ToString("n", CultureInfo.GetCultureInfo("en-US")).Split(' ', '.')[0];
+            => int.Parse(input).ToString("n", CultureInfo.GetCultureInfo("en-US")).Split(',', '.')[0];
     }
 }
